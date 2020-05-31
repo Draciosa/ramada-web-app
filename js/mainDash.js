90adf023e8f260c26e007b8898cc2844d7fa3655
Draciosa <ghaiss5@yahoo.com>
2020-05-31 22:21:00 +0530

    HOME CHANGED

--- js/mainDash.js
+++ js/mainDash.js
@@ -20,6 +20,8 @@ var deleteBtn;
 var listItems = [];
 var database;
 
+
+
 function setup(){
 
   var firebaseConfig = {
@@ -36,11 +38,15 @@ function setup(){
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
+const auth = firebase.auth();
+const db = firebase.firestore();
+
 database = firebase.database();
 deleteBtn = select('#deleteBtn');
 
   loadFirebase();
 }
+
 
 function loadFirebase() {
   var ref = database.ref("maintenance");
@@ -56,16 +62,17 @@ function gotData(data) {
   var complaints = data.val();
 
   var keys = Object.keys(complaints);
-
+  var serial = 0;
   clearList();
 
   var list = createElement('tr');
   list.parent('data');
 
   for (var i = 0; i < keys.length; i++) {
+    serial = serial + 1;
     var key = keys[i];
     var complaint = complaints[key];
-    $("#data").append("<tr><td>" + complaint.room + "</td><td>" + complaint.complaint + "</td><td><button id='deleteBtn' class='btn btn-outline-danger btn-md'>Delete</button></td></tr>");
+    $("#data").append("<tr><td>" + serial + "</td><td>" + complaint.room + "</td><td>" + complaint.complaint + "</td><td><button id='deleteBtn' class='btn btn-outline-danger btn-md'>Delete</button></td></tr>");
 }
 
 function clearList() {
